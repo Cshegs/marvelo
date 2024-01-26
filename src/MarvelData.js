f622f04670a5450de17f373c59741885db292666
Cshegs <agboolasegun230@gmail.com>
2024-01-26 15:02:12 +0100

    Ist commit

---
+++ src/MarvelData.js
@@ -0,0 +1,96 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Allison Becker",
+    alias: "AB1",
+    img1: "AB1.jpeg",
+    homeworld: "Brazil",
+    abilities: [
+      "Shot-Stopping",
+      "Distribution",
+      "Leadership",
+    ],
+    team: "Liverpool FC",
+    description:
+      "Alisson Becker is a Brazilian professional footballer who plays as a goalkeeper for Liverpool FC in the English Premier League and the Brazil national team.",
+  },
+  {
+    name: "Andy Robertson",
+    alias: "Robbo",
+    abilities: ["Tackling", "Stamina"],
+    team: "Liverpool FC",
+    img1: "Andy Robbo.jpeg",
+    homeworld: "Scotland",
+    description:
+      "Andy Robertson is a professional footballer who plays as a left-back for Liverpool and the Scotland national team.",
+  },
+  {
+    name: "Luis Diaz",
+    alias: "Lucho",
+    abilities: ["pace", "dribbling ability"],
+    team: "Avengers",
+    img1: "Luis Diaz.jpeg",
+    homeworld: "Colombia",
+
+    description:
+      "Luis Díaz is a Colombian professional footballer who plays as a winger for Liverpool Football Club and the Colombia national team.",
+  },
+  {
+    name: "Mac Allister",
+    alias: "Allister",
+    abilities: ["versatility", "strength"],
+    team: "Liverpool FC",
+    img1: "Mac Allister.jpeg",
+    homeworld: "Earth",
+
+    description:
+      "Alexis Mac Allister is a Liverpool Football club and Argentinine professional footballer.",
+  },
+
+  // Spider-Man
+  {
+    name: "Mo Salah",
+    alias: "Mo",
+    abilities: ["Pace and Agility", "Finishing and Goal Scoring"],
+    team: "Liverpool FC",
+    img1: "Mo salah.jpeg",
+    homeworld: "Egypt",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      " Mo Salah, is a prolific Egyptian footballer known for his exceptional skills and contributions to the sport.",
+  },
+
+  {
+    name: "Trent Alexander Arnold",
+    alias: "TAA",
+    abilities: ["Passing", "Football Intelligence"],
+    team: "Liverpool FC",
+    img1: "TAA.jpeg",
+    homeworld: "Liverpool",
+    description:
+      "Trent Alexander-Arnold is a highly talented English professional footballer known for his exceptional skills on the field, particularly as a right-back.",
+  },
+
+  {
+    name: "Thiago Alcantara",
+    alias: "Thiago",
+    abilities: ["Ball Control and Dribbling", "Vision and Creativity"],
+    team: "Liverpool FC",
+    img1: "Thiago Alcantara.jpeg",
+    homeworld: "Spain",
+    description:
+      "Thiago Alcântara is a highly skilled and versatile football midfielder known for his exceptional technical abilities and vision on the field.",
+  },
+  {
+    name: "Virgil Van Dijk",
+    alias: "Big Virg",
+    abilities: ["Defensive Rock", "Reading the Game"],
+    team: "Liverpool FC",
+    img1: "VVD.jpeg",
+    homeworld: "Netherlands",
+    description:
+      "Virgil van Dijk is a Dutch professional footballer who's widely regarded as one of the best defenders in the world.",
+  },
+];
+
+console.log(characters);
